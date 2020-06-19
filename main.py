f03575677f0854aefb58c7afdf22b34aba2147bd
Gonzalo Alonso <gonalosos@alum.us.es>
2020-06-19 12:23:42 +0200

    input dataframe

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print('This is the main assignment')
+#For the dataset i will use Pandas DataFrame library
+import pandas as pd
+
+#Input data for the dataframe
+data = {'Age':['Young', 'Young', 'Young', 'Young','Young','Young','Young','Young'
+                ,'Pre-presbyopia','Pre-presbyopia','Pre-presbyopia','Pre-presbyopia'
+                ,'Pre-presbyopia','Pre-presbyopia','Pre-presbyopia','Pre-presbyopia'
+                ,'Presbyopia','Presbyopia','Presbyopia','Presbyopia','Presbyopia'
+                ,'Presbyopia','Presbyopia','Presbyopia'],
+        'Prescription':['Myopia','Myopia','Myopia','Myopia','Hypermetropia',
+                        'Hypermetropia','Hypermetropia','Hypermetropia','Myopia',
+                        'Myopia','Myopia','Myopia','Hypermetropia','Hypermetropia',
+                        'Hypermetropia','Hypermetropia','Myopia','Myopia','Myopia',
+                        'Myopia','Hypermetropia','Hypermetropia','Hypermetropia',
+                        'Hypermetropia'],
+        'Astigmatism':['-','-','+','+','-','-','+','+','-','-','+','+','-','-','+','+'
+                        ,'-','-','+','+','-','-','+','+'],
+        'Tear_rate':['Reduced','Normal','Reduced','Normal','Reduced','Normal',
+                    'Reduced','Normal','Reduced','Normal','Reduced','Normal',
+                    'Reduced','Normal','Reduced','Normal','Reduced','Normal',
+                    'Reduced','Normal','Reduced','Normal','Reduced','Normal',],
+        'Lenses':['None','Soft','None','Hard','None','Soft','None','Hard'
+                    ,'None','Soft','None','Hard','None','Soft','None','None','None'
+                    ,'None','None','Hard','None','Soft','None','None']}
+ 
+# Create DataFrame
+df = pd.DataFrame(data)
+# Print the output.
+print(df)
